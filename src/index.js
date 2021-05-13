2844fbc1c0c3289c0a5aeaafe307693ac90cf652
Clement Liscoet <clement.liscoet@gmail.com>
2021-05-13 22:18:11 +0100

    feat: all funct complete

--- src/index.js
+++ src/index.js
@@ -1,13 +1,13 @@
 import _tool from "fp-dom-tool";
 import "./style.scss";
 
-const alertContainer = (alerTtype) =>
+const _alertContainer = (alerTtype) =>
   _tool._createElement("div")([
     "class",
     `_message__container _message--${alerTtype}`,
   ]);
 
-const alertMessage = _tool._createElementContent("p")([
+const _alertMessage = _tool._createElementContent("p")([
   "class",
   "_message__text",
 ]);
@@ -16,18 +16,51 @@ const compose = (alertContainer, alertMessage) => (alerTtype) => (content) => {
   return _tool._appendElement(alertContainer(alerTtype))(alertMessage(content));
 };
 
-const _alert = compose(alertContainer, alertMessage);
-const _alertDanger = alert("danger");
-const _alertSuccess = alert("success");
-const _alertWarning = alert("warning");
-const _messageContainer = _tool._getElementClass("_message");
-const _switchAlertDisplay = _tool._switchElementDisplay(messageEl);
+const _alert = compose(_alertContainer, _alertMessage);
+const _alertDanger = _alert("danger");
+const _alertSuccess = _alert("success");
+const _alertWarning = _alert("warning");
+
+const _AlertCss = {
+  padding: ".6rem",
+  border: "2px solid #000",
+  borderRadius: "0.3em",
+  fontFamily: "'Montserrat', sans-serif",
+  fontSize: "1.1rem",
+  margin: "0.3rem 0",
+  backgroundColor: "#fff",
+  borderColor: "#000",
+  color: "#000",
+};
+
+const _assignProperty = (cloneAlert) => (newAlert) => (key) => {
+  const newProperty = { [key]: newAlert[key] };
+  return { ...cloneAlert, ...newProperty };
+};
+
+const _composeAlertStyle = (Alert) => (newAlert) => {
+  return Object.keys(newAlert).reduce(
+    (cloneAlert, key) => {
+      return Alert.hasOwnProperty(key)
+        ? _assignProperty(cloneAlert)(newAlert)(key)
+        : cloneAlert;
+    },
+    { ...Alert }
+  );
+};
+
+const _createAlertStyle = _composeAlertStyle(_AlertCss);
+
+const _setStyle = (element) => (newAlertCss) => {
+  Object.assign(element.style, newAlertCss);
+  return element;
+};
 
 module.exports = {
   _alert,
   _alertDanger,
   _alertSuccess,
   _alertWarning,
-  _messageContainer,
-  _switchAlertDisplay,
+  _createAlertStyle,
+  _setStyle,
 };
